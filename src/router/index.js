2fed6fb0deecbd9474771a13e2adc0336d3d7c93
Shino <shino@billows.com.tw>
2024-12-13 11:41:26 +0800

    feat: update route mode and base url

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 
 import AppLayout from '@/layout/AppLayout.vue';
 
@@ -152,7 +152,7 @@ const routes = [
 
 const router = createRouter({
     // history: createWebHistory(import.meta.env.BASE_URL),
-    history: createWebHistory(),
+    history: createWebHashHistory(import.meta.env.BASE_URL),
     routes,
 });
 
