2b8e9ec388cacd16fb80845ab3ae6296536e1411
Shino <shino@billows.com.tw>
2024-12-13 11:41:07 +0800

    feat: update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -14,11 +14,20 @@ npm run build
 # 移動至到打包後的 dist 目錄 
 cd dist
 
-# 因為 dist 資料夾預設是被 ignore 的，因此在進入 dist 資料夾後初始化 git
-git init 
+# 初始化 git，將 dist 資料夾進版控
+git init
+# 切換到 master branch，沒有的話，會自動建立
+git checkout -b gh-pages
+# 將所有檔案加進版控
 git add -A
+# 進行 commit
 git commit -m 'deploy'
-git push -f https://github.com/shiiinnooo/table-admin.git master:gh-pages
+# 加 GitHub 遠端的 repo
+git remote add origin git@github.com:shiiinnooo/table-admin.git
+# 進行強制 push
+git push -f origin gh-pages
+
+# 回到上次的資料夾
 cd -
 
 # 執行指令，在終端機輸入 sh deploy.sh
